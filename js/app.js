11e3484c44ad42a53b5144bf861f7f9449eb903b
shockwave-r <52717105+shockwave-r@users.noreply.github.com>
2019-11-18 11:02:23 -0500

    Premier jet

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function init() {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop(),
+            dh = $(document).height(),
+            wh = $(window).height();
+        scrollPercent = (scroll / (dh - wh)) * 100;
+        $('#progressbar').css('height', scrollPercent + '%');
+        $('#rocket').css('bottom', scrollPercent + '%');
+    })
+
+    $('#userbutton').click(function () {
+        if ($('#user').css("visibility") == "hidden") {
+            $('#user').css("visibility", "visible");
+        } else {
+            $('#user').css("visibility", "hidden");
+        }
+    })
+}
+
+window.onload = init;

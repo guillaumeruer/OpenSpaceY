bd6e4b94b8098425a5a47fa015c0598e2baeb1f0
shockwave-r <52717105+shockwave-r@users.noreply.github.com>
2019-11-23 14:50:53 -0500

    Ajout du panier (version alpha)

---
+++ js/panier.js
@@ -0,0 +1,4 @@
+window.alert(sessionStorage.getItem("type-vaisseau") + "/" +
+    sessionStorage.getItem("couleur-vaisseau") + "/" +
+    sessionStorage.getItem("aile-vaisseau") + "/" +
+    sessionStorage.getItem("reacteur-vaisseau"))
--- js/vaisseaux.js
+++ js/vaisseaux.js
@@ -1,28 +1,42 @@
-$(document).ready(function(){
-  $("#b2").attr("checked",true)
+$(document).ready(function () {
+  $("#b2").attr("checked", true)
 })
 
 
-$(".bouton-vaisseaux-bouton").on("click",function(event){
-  boutonId= event.target.id;
-  id= boutonId.replace("b","")
+$(".bouton-vaisseaux-bouton").on("click", function (event) {
+  boutonId = event.target.id;
+  id = boutonId.replace("b", "")
 
-  if(id==1){
+  if (id == 1) {
 
     $("#s1").fadeIn();
-    $("#s2").css("display","none");
-    $("#s3").css("display","none");
+    $("#s2").css("display", "none");
+    $("#s3").css("display", "none");
   }
 
-  if(id==2){
-    $("#s1").css("display","none")
+  if (id == 2) {
+    $("#s1").css("display", "none")
     $("#s2").fadeIn();
-    $("#s3").css("display","none")
+    $("#s3").css("display", "none")
   }
 
-  if(id==3){
-    $("#s1").css("display","none")
-    $("#s2").css("display","none")
+  if (id == 3) {
+    $("#s1").css("display", "none")
+    $("#s2").css("display", "none")
     $("#s3").fadeIn();
-  }
-;})
+  };
+})
+
+$('#submitBtn').click(function () {
+  let type = $("input[name='type-vaisseau']:checked").val();
+  let couleur = $("input[name='couleur']:checked").val();
+  let aile = $("input[name='aile']:checked").val();
+  let reacteur = $("input[name='reacteur']:checked").val();
+
+  sessionStorage.setItem("type-vaisseau", type);
+  sessionStorage.setItem("couleur-vaisseau", couleur);
+  sessionStorage.setItem("aile-vaisseau", aile);
+  sessionStorage.setItem("reacteur-vaisseau", reacteur);
+
+  window.location = "panier.html";
+})

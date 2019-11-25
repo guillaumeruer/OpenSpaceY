12886a404df0dc5cb0fb1f3b37c0bb70d2633de0
shockwave-r <52717105+shockwave-r@users.noreply.github.com>
2019-11-25 12:44:24 -0500

    Panier fonctionnel :)

--- js/vaisseaux.js
+++ js/vaisseaux.js
@@ -1,3 +1,6 @@
+let i = 1;
+let j;
+
 $(document).ready(function () {
   $("#b2").attr("checked", true)
 })
@@ -33,10 +36,26 @@ $('#submitBtn').click(function () {
   let aile = $("input[name='aile']:checked").val();
   let reacteur = $("input[name='reacteur']:checked").val();
 
-  sessionStorage.setItem("type-vaisseau", type);
+  let vaisseau = {
+    "type": type,
+    "couleur": couleur,
+    "aile": aile,
+    "reacteur": reacteur
+  }
+
+  if (sessionStorage.getItem("compteur") === "undefined") {
+    sessionStorage.setItem("compteur", i);
+  } else {
+    sessionStorage.compteur = parseInt(sessionStorage.compteur) + 1;
+    j = parseInt(sessionStorage.getItem("compteur"));
+  }
+  let id = "vaisseau" + j;
+
+  sessionStorage.setItem(id, JSON.stringify(vaisseau));
+  /*sessionStorage.setItem("type-vaisseau", type);
   sessionStorage.setItem("couleur-vaisseau", couleur);
   sessionStorage.setItem("aile-vaisseau", aile);
-  sessionStorage.setItem("reacteur-vaisseau", reacteur);
+  sessionStorage.setItem("reacteur-vaisseau", reacteur);*/
 
   window.location = "panier.html";
 })

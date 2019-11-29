3afd5598e34a860140af3d826834c19c41edda77
JessykB <54864317+JessykB@users.noreply.github.com>
2019-11-29 12:19:08 -0500

    design assurances

--- js/script-assurances.js
+++ js/script-assurances.js
@@ -13,17 +13,18 @@ class ass{
 let ass_tiers=new ass("ass_tiers","Assurance au tiers",["Responsabilité civile","Protection juridique en cas d'accident","Garantie du conducteur","Assistance"],47.49);
 let ass_intermediaire=new ass("ass_intermediaire","Assurance intermédiaire",["Responsabilité civile","Protection juridique en cas d'accident","Garantie du conducteur","Assistance","Bris de glace","Vol"],59.15);
 let ass_aventureux=new ass("ass_aventureux","Assurance Aventureux",["Responsabilité civile","Protection juridique en cas d'accident","Garantie du conducteur","Assistance","Incendie","Catastrophes"],63.19);
-let ass_frequentation=new ass("ass_frequentation","Assurance mauvaise fréquentation",["Responsabilité civile","Protection juridique en cas d'accident","Garantie du conducteur","Assistance","Vol","Vandalisme"],65.75);
+let ass_frequentation=new ass("ass_frequentation","Assurance mauvaises fréquentations",["Responsabilité civile","Protection juridique en cas d'accident","Garantie du conducteur","Assistance","Vol","Vandalisme"],65.75);
 let ass_toutRisques=new ass("ass_toutRisques","Assurance tout risques",["Responsabilité civile","Protection juridique en cas d'accident","Garantie du conducteur","Assistance","Bris de glace","Vol","Incendie","Catastrophes","Vandalisme","Dommages tout accidents"],88.53);
 let ass_luxe=new ass("ass_luxe","Assurance luxe",["Responsabilité civile","Protection juridique en cas d'accident","Garantie du conducteur","Assistance","Bris de glace","Vol","Incendie","Catastrophes","Vandalisme","Dommages tout accidents","Véhicule de remplacement","Panne mécanique","Effets personnels"],112.68);
 
 LesAssurances=[ass_tiers,ass_intermediaire,ass_aventureux,ass_frequentation,ass_toutRisques,ass_luxe];
 
 function getAssurance(valeur) {
-    //permet de récupérer la bonne classe de l'assurance via la valeur de l'input
+    //permet de récupérer la bonne classe de l'assurance via la valeur de la checkbox
     let obj=LesAssurances.find(assurance => assurance.val==valeur);
     return obj;
 }
+
 function minPrix(list){
     let min=list[0].prix;
     for(var element of list){
